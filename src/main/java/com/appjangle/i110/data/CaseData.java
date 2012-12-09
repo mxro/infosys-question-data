cf86a71a3aee1809f128ca310043f1801ee0d3df
mxro <mxro@nowhere.com>
2012-12-10 12:09:05 +1300

    improving way types are handled

--- src/main/java/com/appjangle/i110/data/CaseData.java
+++ src/main/java/com/appjangle/i110/data/CaseData.java
@@ -9,34 +9,7 @@ public class CaseData {
 
 	public static void writeCaseData(Session session, Node node) {
 
-		// required data types
-
-		Link brandName = session.node(Types.aBrandName);
-		Link vision = session.node(Types.aVision);
-		Link brandImage = session.node(Types.aBrandImage);
-		Link brandVideo = session.node(Types.aBrandVideo);
-		Link industry = session.node(Types.anIndustry);
-
-		// game 1 types
-		Link correctStrategy = session.node(Types.aCorrectStrategy);
-		Link competitiveScope = session.node(Types.aCompetitiveScope);
-		Link costStrategy = session.node(Types.aCostStrategy);
-
-		// game 2 types
-		Link correctIndustryStructure = session
-				.node(Types.aCorrectIndustryStructure);
-		Link threatOfSubsitutes = session
-				.node(Types.aThreatOfSubsitutesDescription);
-		Link supplierPower = session.node(Types.aSupplierPowerDescription);
-		Link rivarly = session.node(Types.aRivalryAmongCompetitorsDescription);
-		Link buyerPower = session.node(Types.aBuyerPowerDescription);
-		Link newEntrants = session.node(Types.aThreatofNewEntrantsDescription);
-
-		// game 3 types
-		Link correctValueChainActivites = session
-				.node(Types.correctValueChainActivites);
-		Link valueChainJustification = session
-				.node(Types.valueChainJustification);
+		Types t = new Types(session);
 
 		// ---------
 		// CASE 1 STARBUCKS
@@ -44,69 +17,69 @@ public class CaseData {
 		Query case1 = node.append("c1", "./c1");
 
 		// Common case data
-		case1.append("Starbucks", "./brandName").append(brandName);
+		case1.append("Starbucks", "./brandName").append(t.brandName);
 		case1.append(
 				"http://hollywoodandswine.com/wp-content/uploads/2012/03/Starbucks-2.jpg",
-				"./brandImage").append(brandImage);
+				"./brandImage").append(t.brandImage);
 		case1.append(
 				"http://www.youtube.com/watch?v=JagNph-q9LU&feature=relmfu",
-				"./brandVideo").append(brandVideo);
+				"./brandVideo").append(t.brandVideo);
 		case1.append(
 				"To inspire and nurture the human spirit – one person, one cup and one neighborhood at a time.",
-				"./brandVision").append(vision);
-		case1.append("Cafe industry", "./industry");
+				"./brandVision").append(t.vision);
+		case1.append("Cafe industry", "./industry").append(t.industry);
 
 		// Game 1, Strategy
 
 		case1.append("differentiation", "./correctStrategy").append(
-				correctStrategy);
+				t.correctStrategy);
 
 		case1.append(
 				"Starbucks markets to all customers of the cafe industry, which is broad market.",
-				"./competitveScope").append(competitiveScope);
+				"./competitveScope").append(t.competitiveScope);
 
 		case1.append(
 				"Starbucks has a focus on high quality and customer service, indicative of a high cost strategy.",
-				"./costStrategy").append(costStrategy);
+				"./costStrategy").append(t.costStrategy);
 
 		// Game 2, Porter's five forces
 
 		// see
 		// http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Industry_Structure
 		case1.append("high:low:high:high:high", "./industryStructure").append(
-				correctIndustryStructure);
+				t.correctIndustryStructure);
 
 		case1.append(
 				"A substitute to the café industry is to prepare food and drink at home, or purchase from a supermarket.",
-				"./threatOfSubsitutes").append(threatOfSubsitutes);
+				"./threatOfSubsitutes").append(t.threatOfSubsitutes);
 
 		case1.append(
 				"Suppliers provide mainly commodity products, such as milk and coffee beans, where there are plenty of alternative suppliers to choose from.",
-				"./supplierPower").append(supplierPower);
+				"./supplierPower").append(t.supplierPower);
 
 		case1.append(
 				"There are many participants in this industry, all doing similar things. Rivalry can be reduced through innovation and differentiation.",
-				"./rivarly").append(rivarly);
+				"./rivarly").append(t.rivarly);
 
 		case1.append(
 				"Buyer power can be reduced through loyalty programmes, product differentiation, and switching costs.",
-				"./buyerPower").append(buyerPower);
+				"./buyerPower").append(t.buyerPower);
 
 		case1.append(
 				"There are low start-up costs required to enter the cafe industry.",
-				"./newEntrants").append(newEntrants);
+				"./newEntrants").append(t.newEntrants);
 
 		// Game 3, Value chain
 
 		// see http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Value_Chain_Activites
 		case1.append("procurement,makeProjectOrService,technologyDevelopment",
-				"./valueChainActivites").append(correctValueChainActivites);
+				"./valueChainActivites").append(t.correctValueChainActivites);
 
 		case1.append(
 				"Procurement: Starbucks needs to get the best quality, certified fairtrade coffee beans.\n"
 						+ "Make the product or service: Starbucks’ strategy involves quality, and pleasing people – this is achieved through providing the best possible cup of coffee\n"
 						+ "Research and Development: In order to differentiate their product, and compete with rivals,  Starbucks must continually produce new offerings. ",
-				"./valueChainJustification").append(valueChainJustification);
+				"./valueChainJustification").append(t.valueChainJustification);
 
 		// ---------
 		// CASE 2 NIKE
@@ -114,67 +87,67 @@ public class CaseData {
 		Query case2 = node.append("c2", "./c2");
 
 		// Common case data
-		case2.append("Nike", "./brandName").append(brandName);
+		case2.append("Nike", "./brandName").append(t.brandName);
 		case2.append("http://vince08.edublogs.org/files/2008/11/logonike.gif",
-				"./brandImage").append(brandImage);
+				"./brandImage").append(t.brandImage);
 		case2.append("http://www.youtube.com/watch?v=45mMioJ5szc",
-				"./brandVideo").append(brandVideo);
+				"./brandVideo").append(t.brandVideo);
 		case2.append(
-				"To bring inspiration and innovation to every athlete* in the world.",
-				"./brandVision").append(vision);
-		case2.append("Sporting Apparel Footwear and Accessories", "./industry");
+				"To bring inspiration and innovation to every athlete in the world.",
+				"./brandVision").append(t.vision);
+		case2.append("Sporting Apparel Footwear and Accessories", "./industry").append(t.industry);
 
 		// Game 1, Strategy
 
 		case2.append("differentiation", "./correctStrategy").append(
-				correctStrategy);
+				t.correctStrategy);
 
 		case2.append(
 				"Broad market – Nike appeals to a wide range of diverse consumers at all ends of the spectrum.",
-				"./competitveScope").append(competitiveScope);
+				"./competitveScope").append(t.competitiveScope);
 
 		case2.append(
 				"High cost – Nike sells high quality products and therefore can sell at inflated prices. It also attempts to use its brand name as a distinctive selling point.",
-				"./costStrategy").append(costStrategy);
+				"./costStrategy").append(t.costStrategy);
 
 		// Game 2, Porter's five forces
 
 		// see
 		// http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Industry_Structure
 		case2.append("low:low:high:high:high", "./industryStructure").append(
-				correctIndustryStructure);
+				t.correctIndustryStructure);
 
 		case2.append(
 				"In this day and age where nearly everyone who engages in physical activity ‘must have’ appropriate gear, there is no adequate substitute for sporting apparel footwear.",
-				"./threatOfSubsitutes").append(threatOfSubsitutes);
+				"./threatOfSubsitutes").append(t.threatOfSubsitutes);
 
 		case2.append(
 				"Most of the raw materials used in this industry are not difficult to procure, and are widely available.",
-				"./supplierPower").append(supplierPower);
+				"./supplierPower").append(t.supplierPower);
 
 		case2.append(
 				"There is a lot of competition between the already established companies as they have to distinguish between very similar products. Brand loyalty is very high and so competitors struggle to gain market share.",
-				"./rivarly").append(rivarly);
+				"./rivarly").append(t.rivarly);
 
 		case2.append(
 				"In this day and age there are many players in the industry that make what are traditionally similar products. However the players in the industry seek to reduce this by product innovation.",
-				"./buyerPower").append(buyerPower);
+				"./buyerPower").append(t.buyerPower);
 
 		case2.append(
 				"The technology behind the apparel can be easily manufactured. However, the existing brand loyalty shown by customers will make it extremely hard for any new entrant to gain traction once in the market place.",
-				"./newEntrants").append(newEntrants);
+				"./newEntrants").append(t.newEntrants);
 
 		// Game 3, Value chain
 
 		// see http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Value_Chain_Activites
 		case2.append("marketing,makeProjectOrService,technologyDevelopment",
-				"./valueChainActivites").append(correctValueChainActivites);
+				"./valueChainActivites").append(t.correctValueChainActivites);
 
 		case2.append(
 				"Market and Sell the Product or Service: As shown in the video above, Nike relies on its marketing to enhance its famous brand image (which is its highest selling point). Nike focuses on the importance of self-belief and true effort in order to be the best, and attempts to show how their products can take you there.\n"
 						+ "Make the product or service: Nike has to ensure that its products are of the highest quality to ensure that it stands up to the image that it portrays in its marketing strategy.\n"
 						+ "Research and Development: In order to differentiate their product, and compete with rivals, Nike must continually produce new offerings. ",
-				"./valueChainJustification").append(valueChainJustification);
+				"./valueChainJustification").append(t.valueChainJustification);
 
 		session.commit().get();
 
--- src/main/java/com/appjangle/i110/data/Types.java
+++ src/main/java/com/appjangle/i110/data/Types.java
@@ -1,6 +1,70 @@
 package com.appjangle.i110.data;
 
+import io.nextweb.Link;
+import io.nextweb.Session;
+
 public class Types {
+
+	public Link brandName;
+
+	public Link vision;
+
+	public Link brandImage;
+
+	public  Link brandVideo;
+
+	public  Link industry;
+
+	public Link correctStrategy;
+
+	public  Link competitiveScope;
+
+	public Link costStrategy;
+
+	public  Link correctIndustryStructure;
+
+	public Link threatOfSubsitutes;
+
+	public  Link supplierPower;
+
+	public  Link rivarly;
+
+	public Link buyerPower;
+
+	public  Link newEntrants;
+
+	public  Link correctValueChainActivites;
+
+	public  Object valueChainJustification;
+
+	public Types(Session session) {
+		// required data types
+
+		brandName = session.node(Types.aBrandName);
+		vision = session.node(Types.aVision);
+		brandImage = session.node(Types.aBrandImage);
+		brandVideo = session.node(Types.aBrandVideo);
+		industry = session.node(Types.anIndustry);
+
+		// game 1 types
+		correctStrategy = session.node(Types.aCorrectStrategy);
+		competitiveScope = session.node(Types.aCompetitiveScope);
+		costStrategy = session.node(Types.aCostStrategy);
+
+		// game 2 types
+		correctIndustryStructure = session
+				.node(Types.aCorrectIndustryStructure);
+		threatOfSubsitutes = session.node(Types.aThreatOfSubsitutesDescription);
+		supplierPower = session.node(Types.aSupplierPowerDescription);
+		rivarly = session.node(Types.aRivalryAmongCompetitorsDescription);
+		buyerPower = session.node(Types.aBuyerPowerDescription);
+		newEntrants = session.node(Types.aThreatofNewEntrantsDescription);
+
+		// game 3 types
+		correctValueChainActivites = session
+				.node(Types.aCorrectValueChainActivites);
+		valueChainJustification = session.node(Types.aValueChainJustification);
+	}
 
 	/**
 	 * @see <a
@@ -18,16 +82,15 @@ public class Types {
 	public static final String aBrandImage = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Brand_Image";
 
 	public static final String aBrandVideo = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Video_Link";
-	
+
 	public static final String anIndustry = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Industry";
 
-	
 	// Game 1
 
 	public static final String aCorrectStrategy = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Correct_Strategy";
 
 	public static final String aCompetitiveScope = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Competitive_Scope";
-	
+
 	public static final String aCostStrategy = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Cost_Strategy";
 
 	// Game 2
@@ -45,8 +108,8 @@ public class Types {
 	public static final String aSupplierPowerDescription = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Supplier_Power";
 
 	// Game 3
-	
-	public static final String correctValueChainActivites =  "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Value_Chain_Activites";
 
-	public static final String valueChainJustification = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Activities_Justiciation";
+	public static final String aCorrectValueChainActivites = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Value_Chain_Activites";
+
+	public static final String aValueChainJustification = "http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Activities_Justiciation";
 }
